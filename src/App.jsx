cfa9616aa4b1f7ee34ef91804a458dcba5400fae
keplib <keplib@gmail.com>
2022-11-25 13:20:44 +0100

    Establish connection to API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all').then(response => {
+        setCountries(response.data)
+        console.log(response.data);
+      })
+  }, [])
 
   return (
     <div>
       <p>Hello</p>
+      {countries.map(country => <p>{country.name.common}</p>
+      )}
     </div>
   )
 }
