7c9707f5c9ef19052a58c9c76d7e3a9f432c57c0
keplib <keplib@gmail.com>
2022-11-29 17:16:12 +0100

    Part2: 2.14 SOLUTION

--- src/components/WeatherSection.jsx
+++ src/components/WeatherSection.jsx
@@ -6,20 +6,34 @@ const WeatherSection = ({ country }) => {
     const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
     const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${API_KEY}`
 
-    const [weatherData, setWeatherData] = [{}];
+
+    const [weatherData, setWeatherData] = useState([]);
 
     useEffect(() => {
         axios
             .get(WEATHER_URL).then(response => {
-                // setWeatherData(response.data)
-                console.log(response.data);
+                const apiResponse = response.data;
+                setWeatherData([apiResponse])
+                console.log(weatherData);
             })
     }, [])
 
+    if (weatherData.length > 0) {
+        return (
+            <div>
+                {/* <h3> Weather in {country.capital}</h3>
+                <p>General condition: {weatherData.weather[0].description}</p>
+                <p>Temperature: {weatherData.main.temp}</p>
+                <p>Wind: {weatherData.wind.speed}</p> */}
+                <h1>WEATHER DATA IS LOADED</h1>
+            </div>
+        )
+    }
+
     return (
         <div>
-            <p>WeatherSection</p>
             <h3> Weather in {country.capital}</h3>
+            <p>Weather data could not be loaded</p>
         </div>
     )
 }
